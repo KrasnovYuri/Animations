52132c0d1a4c2d7487aae70909ecb26af323289b
KrasnovYuri <krasnov1yuri@gmail.com>
2024-02-21 18:44:23 +0300

    start project

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Юрий on 21.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
